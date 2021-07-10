65e2a8161336c25b22d7e7e1cf72c46e38fabcc9
Danny Harpigny <dannyhpy@disroot.org>
2021-07-10 07:19:22 +0200

    W.I.P.: Add DeveloperAccount, login(email, pw) method

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -9,6 +9,24 @@ declare module 'brawlstars.js' {
     token: string
   }
 
+  interface DeveloperAccProperties {
+    developer: {
+      id: string
+      name: string
+      email: string
+      prevLoginIp: string
+    }
+    temporaryAPIToken: string
+    // ...
+  }
+
+  interface KeyProperties {
+    ip: string
+    name: string
+    token: string
+    // ...
+  }
+
   interface PlayerProperties {
     tag: string
     // ...
@@ -40,6 +58,8 @@ declare module 'brawlstars.js' {
     constructor (token: string)
     constructor (options: ClientOptions)
 
+    readonly dev?: DeveloperAccount
+
     readonly players: PlayerManager
     readonly clubs: ClubManager
     readonly brawlers: BrawlerManager
@@ -50,6 +70,47 @@ declare module 'brawlstars.js' {
      * See https://developer.brawlstars.com/ to get a token.
      */
     token: string
+  }
+
+  export class DeveloperAccount {
+    constructor (client: Client, props: DeveloperAccProperties)
+    readonly client: Client
+
+    readonly keys: KeyManager
+
+    readonly id: string
+    readonly ip: string
+    readonly name: string
+    readonly email: string
+    readonly token: string
+  }
+
+  export class KeyManager {
+    constructor (client: Client)
+    readonly client: Client
+
+    create (/* ... */): Promise<Key>
+
+    delete (/* ... */): Promise<void>
+  }
+
+  export class Key {
+    constructor (client: Client, props: KeyProperties)
+    readonly client: Client
+
+    readonly ip: string
+    readonly name: string
+    readonly token: string
+
+    /**
+     * Use this key to authenticate
+     * to the BrawlStars API.
+     *
+     * Same as `client.setToken(key.token)`.
+     */
+    use (): void
+
+    delete (): Promise<void>
   }
 
   export class PlayerManager {
